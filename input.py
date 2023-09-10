b06753e8eda3f84ea0073e6ff7a82d4776e9cc35
John Paulo Battung <126607972+sSquishy@users.noreply.github.com>
2023-09-10 23:02:41 +0800

    Update input.py

--- input.py
+++ input.py
@@ -10,4 +10,18 @@ print("Character input:", char_input, "Type:", type(char_input))
 int_input = int(input("Enter an integer: "))
 print("Integer input:", int_input, "Type:", type(int_input))
 
+# Float input
+float_input = float(input("Enter a float: "))
+print("Float input:", float_input, "Type:", type(float_input))
 
+# Tuple input
+tuple_input = tuple(input("Enter three elements for a tuple: "))
+print("Tuple input:", tuple_input, "Type:", type(tuple_input))
+
+# List input
+list_input = list(input("Enter three elements for a list: "))
+print("List input:", list_input, "Type:", type(list_input))
+
+# Complex input
+complex_input = input("Enter a complex number (in the form of a+bj): ")
+print("Complex input:", complex_input, "Type:", type(complex_input))
